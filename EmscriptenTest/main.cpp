82eb997bfd8bb9bc3e3c6d46059feb2fb0424380
lhw0507 <lhw0507>
2022-12-13 15:29:24 +0900

    Emscripten test (opengl tutorial red triangle)

---
+++ EmscriptenTest/main.cpp
@@ -0,0 +1,137 @@
+// Include standard headers
+//#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+
+#include "shader.hpp"
+#include <emscripten/emscripten.h>
+
+struct programInfo
+{
+	GLuint programID;
+	GLuint vertexbuffer;
+};
+
+void do_frame(void* pProgramInfo)
+{
+	auto* tProgramInfo = (programInfo*)pProgramInfo;
+
+	// Clear the screen
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Use our shader
+	glUseProgram(tProgramInfo->programID);
+
+	// 1rst attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, tProgramInfo->vertexbuffer);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	// Draw the triangle !
+	glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+	glDisableVertexAttribArray(0);
+
+	// Swap buffers
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+}
+
+int main(void)
+{
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		//fprintf(stderr, "Failed to initialize GLFW\n");
+		//getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+	if (window == NULL) {
+		//fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		std::cout << "error1" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		//fprintf(stderr, "Failed to initialize GLEW\n");
+		std::cout << "error2" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	//glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("asset/SimpleVertexShader.vertexshader", "asset/SimpleFragmentShader.fragmentshader");
+
+	if (programID == 0)
+	{
+		std::cout << "fail" << std::endl;
+		return 0;
+	}
+
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	std::cout << "before while loop" <<std::endl;
+
+	programInfo tProgramInfo = { programID , vertexbuffer};
+
+	emscripten_set_main_loop_arg(do_frame, &tProgramInfo, 0, 1);
+
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
